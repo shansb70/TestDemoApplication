25a8d16dca63a67febcce6e6d04e8475016c6245
shanmuga <sundar_sb70@hotmail.com>
2022-02-17 23:08:00 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        //[HttpPost]
+        public IActionResult CreateUser(UserDetail userDetail)
+        {
+            Repository.Create(userDetail);
+
+       
+            return View();
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Repository
+    {
+        internal static void Create(UserDetail userDetail)
+        {
+            _ = new UserDetail
+            {
+                FirstName = userDetail.FirstName,
+                LastName = userDetail.LastName
+            };
+
+      
+            List<UserDetail> fdata = new List<UserDetail>();
+            fdata.Add(userDetail);
+            String json = JsonConvert.SerializeObject(fdata.ToArray());
+            File.WriteAllText(@"D:\TEST2.CSV", json);
+
+
+        }
+    }
+}
